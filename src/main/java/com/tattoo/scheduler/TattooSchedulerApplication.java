e0ef8afe615ca6a4a0e011f595a6f5e0c68db212
Edman <EdmanGonnaCry@gmail.com>
2026-03-03 12:11:08 +0300

    Initial project setup with Docker and Srping Boot

---
+++ src/main/java/com/tattoo/scheduler/TattooSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.tattoo.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TattooSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TattooSchedulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tattoo/scheduler/TattooSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tattoo.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TattooSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
